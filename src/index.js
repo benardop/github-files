cd8672f0ad0222bbf28dac6f1f9f609d1efbd666
benardop <benard.oc.p@gmail.com>
2023-11-22 19:00:02 -0300

    Initail commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,59 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import PropTypes from 'prop-types';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const FileList = ({files}) => (
+  <table className='filelist'>
+    <tbody>
+      {files.map(file => (
+        <tr className='filelist-item' key={file.id}>
+           <td className='file-name'>{file.name}</td>
+        </tr>
+      ))}
+    </tbody>
+  </table>
+);
+
+FileList.prototype = {
+  files:PropTypes.Array
+}
+
+const testFiles = [
+  {
+    id: 1,
+    name: 'src',
+    type: 'folder',
+    updated_at: '2023-11-22 21:35:00',
+    latestCommit: {
+      message: 'initial commit'
+    }
+  },
+  {
+    id: 2,
+    name: 'tests',
+    type: 'folder',
+    updated_at: '2023-11-22 21:35:00',
+    latestCommit: {
+      message: 'initial commit'
+    }
+  },
+  {
+    id: 3,
+    name: 'README',
+    type: 'file',
+    updated_at: '2023-11-22 21:35:00',
+    latestCommit: {
+      message: 'Add a readme file'
+    }
+  }
+
+
+];
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <FileList files={testFiles}/>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
