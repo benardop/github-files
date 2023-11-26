984613b341bde3afc09419d03b3916d3d67243b4
benardop <benard.oc.p@gmail.com>
2023-11-26 18:54:32 -0300

    Update index file

--- src/index.js
+++ src/index.js
@@ -97,9 +97,39 @@ const testFiles = [
 
 ];
 
+class Parent extends React.Component {
+  state = {
+    actionCount: 0
+  }
+
+  handleAction = (action) => {
+    console.log('child says', action);
+    this.setState({
+     actionCount: this.state.actionCount + 1
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <Child onAction={this.handleAction}/>
+        <p>Clicked {this.state.actionCount} times</p>
+      </div>
+    )
+  }
+}
+
+function Child({onAction}){
+  return (
+    <button onClick={onAction}>
+        Click me!
+    </button>
+  )
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <FileList files={testFiles}/>
+    <Parent />
   </React.StrictMode>
 );
